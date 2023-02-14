9a622689733eacaa61f04fdecac1a072b6cf92d3
김현서 <hyunseo5918@naver.com>
2023-02-14 18:01:16 +0900

    v3

--- src/ThreadThree.java
+++ src/ThreadThree.java
@@ -1,2 +1,26 @@
+import java.util.concurrent.*;
+
 public class ThreadThree {
+    public static void main(String[] args) {
+        int alphabet = 'a';
+        Runnable task = () -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("작업 스레드 : " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(task);
+
+        while (exec.isShutdown() == false) {
+            System.out.println("메인 스레드 : " + (char) alphabet++);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
 }
